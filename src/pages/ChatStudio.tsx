a9ca8fbaf0e3c42a6efd7fcf188666105bbe124c
Bmb Tech <hanchobmb@gmail.com>
2026-07-30 19:00:42 +0300

    Update ChatStudio.tsx

--- src/pages/ChatStudio.tsx
+++ src/pages/ChatStudio.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { ChatTools } from '../components/ChatTools';
 
-const ChatStudio: React.FC = () => {
-  return <ChatTools />;
+interface ChatStudioProps {
+  activeChatId: string | null;
+  loadChatTrigger: number;
+  onChatIdChange: (id: string | null) => void;
+}
+
+const ChatStudio: React.FC<ChatStudioProps> = ({ activeChatId, loadChatTrigger, onChatIdChange }) => {
+  return <ChatTools activeChatId={activeChatId} loadChatTrigger={loadChatTrigger} onChatIdChange={onChatIdChange} />;
 };
 
 export default ChatStudio;
